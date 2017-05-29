3629525113b89cca1a6994af9ecb964d1c18bd74
Saverio <Saverio@SAVERIO-PC>
2017-05-29 11:11:39 +0200

    commit3

--- ProgettoMalnati1/MainWindow.xaml.cs
+++ ProgettoMalnati1/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace ProgettoMalnati1
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            //hhhhh
-        }
+            
+        }//hhhhh
     }
 }
