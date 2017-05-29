73c36aebeb933f2fe09da074514a6c26448c8337
Saverio <Saverio@SAVERIO-PC>
2017-05-29 13:13:31 +0200

    metodo sendTcp

--- ProgettoMalnati1/Client.cs
+++ ProgettoMalnati1/Client.cs
@@ -8,6 +8,9 @@ using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Net;
 using System.Threading;
+using System.IO;
+using ProgettoMalnati1;
+using System.Windows.Controls;
 
 namespace ProgettoMalnati1
 {
@@ -20,9 +23,68 @@ namespace ProgettoMalnati1
 
         public void startBroadcast()
         {
+            /*
             UdpClient newClient = new UdpClient();
 
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp );
+            IPEndPoint ip = new IPEndPoint(IPAddress.Broadcast, 15000);
+
+            byte[] bytes = Encoding.ASCII.GetBytes("richiesta");
+            newClient.Send(bytes, bytes.Length, ip);//invio paccheti broadcast
+            newClient.Close();
+            */
 
         }
+        public void sendFileTCP(string IP_addr, Int32 port_number, string nome_file)
+        {
+            /**************
+            cancellare la classe progressbar già implementata in windows
+            **********/
+            try
+            {
+                TcpClient client = new TcpClient(IP_addr, port_number);
+                NetworkStream netstream = client.GetStream();
+                int BufferSize = 1024;/**dimensione del pacchetto inviato**/
+
+                FileStream Fs = new FileStream(nome_file, FileMode.Open, FileAccess.Read);/**apro il file in lettura**/
+                int packets_number = Convert.ToInt32
+         (Math.Ceiling(Convert.ToDouble(Fs.Length) / Convert.ToDouble(BufferSize)));
+                /**il numero di pacchetti da inviare è pari alla grandezza totale del file diviso la 
+            grandezza del buffer**/
+
+                ProgressBar progress_bar = new ProgressBar();
+                progress_bar.setMaximum(packets_number);
+
+                int lunghezza_file = (int)Fs.Length, dim_pacchetto_corrente, cont = 0;
+                
+                for(int i = 0; i < packets_number; i++)
+                {
+                    if (lunghezza_file > BufferSize)
+                    {
+                        dim_pacchetto_corrente = BufferSize;
+                        lunghezza_file = lunghezza_file - dim_pacchetto_corrente;
+                    }
+                    else
+                        dim_pacchetto_corrente = lunghezza_file;
+                    byte[] SendingBuffer = new byte[dim_pacchetto_corrente];
+                    Fs.Read(SendingBuffer, 0, dim_pacchetto_corrente);
+                    netstream.Write(SendingBuffer, 0, (int)SendingBuffer.Length);
+                    /*if (progressBar1.Value >= progressBar1.Maximum)
+                        progressBar1.Value = progressBar1.Minimum;
+                    progressBar1.PerformStep();*/
+
+                }
+                
+                 
+            }
+            catch(Exception e)
+
+            {
+
+
+            }
+            finally {   }
+        }
+    
     }
 }
