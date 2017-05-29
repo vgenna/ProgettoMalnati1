7345e4e8525384b4d288547eb549f8884a4f5b7e
Vincenzo <vcn.genna@gmail.com>
2017-05-29 13:11:53 +0200

    Iniziato codice per UDP discovery

--- ProgettoMalnati1/Client.cs
+++ ProgettoMalnati1/Client.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 
 using System.Net.Sockets;
@@ -20,9 +21,22 @@ namespace ProgettoMalnati1
 
         public void startBroadcast()
         {
-            UdpClient newClient = new UdpClient();
+            UdpClient newClient = new UdpClient(); //client per inviare il messaggio broadcast
+            byte[] requestData = Encoding.ASCII.GetBytes("Request Data"); //dati da inviare in broadcast
+            IPEndPoint remoteServer = new IPEndPoint(IPAddress.Any, 0); //server da cui ricevere le risposte
+
+            newClient.EnableBroadcast = true;
+            newClient.Send(requestData, requestData.Length, new IPEndPoint(IPAddress.Broadcast,1500));
 
 
+            byte[] serverResponseBytes = newClient.Receive(ref remoteServer);
+            string serverResponse = Encoding.ASCII.GetString(serverResponseBytes);
+
+            string formattedString = string.Format("Client: Recieved '{0}' from {1}", serverResponse, remoteServer.Address.ToString());
+
+            MessageBox.Show(formattedString);
+
+            newClient.Close();
         }
     }
 }
