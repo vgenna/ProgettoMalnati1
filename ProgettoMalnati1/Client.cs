1ed0511f9de681d12d26a86ebd9118c4a8722f1c
Vincenzo <vcn.genna@gmail.com>
2017-05-29 11:02:02 +0200

    Create classi Client  e Server

---
+++ ProgettoMalnati1/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace ProgettoMalnati1
+{
+    class Client
+    {
+        public Client()
+        {
+
+        }
+
+        public void startBroadcast()
+        {
+           UdpClient newClient = new UdpClient();
+        }
+    }
+}
--- ProgettoMalnati1/MainWindow.xaml.cs
+++ ProgettoMalnati1/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ProgettoMalnati1
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
     }
 }
