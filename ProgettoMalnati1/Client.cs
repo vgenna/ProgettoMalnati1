b6392ab1d29239283157a1009e50efd9bab49df8
Vincenzo <vcn.genna@gmail.com>
2017-06-02 11:50:32 +0200

    Risolta ricezione multipla di pacchetti UDP al client

--- ProgettoMalnati1/Client.cs
+++ ProgettoMalnati1/Client.cs
@@ -27,81 +27,35 @@ namespace ProgettoMalnati1
             UdpClient newClient = new UdpClient(); //client per inviare il messaggio broadcast
             byte[] requestData = Encoding.ASCII.GetBytes("Request Data"); //dati da inviare in broadcast
             IPEndPoint remoteServer = new IPEndPoint(IPAddress.Any, 0); //server da cui ricevere le risposte
+            newClient.Client.ReceiveTimeout = 3000;
 
             newClient.EnableBroadcast = true;
-            newClient.Send(requestData, requestData.Length, new IPEndPoint(IPAddress.Broadcast,1500));
+            newClient.Send(requestData, requestData.Length, new IPEndPoint(IPAddress.Broadcast, 1500));
 
 
-            byte[] serverResponseBytes = newClient.Receive(ref remoteServer);
-            string serverResponse = Encoding.ASCII.GetString(serverResponseBytes);
-
-            string formattedString = string.Format("Client: Recieved '{0}' from {1}", serverResponse, remoteServer.Address.ToString());
-            /*
-            UdpClient newClient = new UdpClient();
-
-            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp );
-            IPEndPoint ip = new IPEndPoint(IPAddress.Broadcast, 15000);
-
-            byte[] bytes = Encoding.ASCII.GetBytes("richiesta");
-            newClient.Send(bytes, bytes.Length, ip);//invio paccheti broadcast
-            newClient.Close();
-            */
-
-        }
-        public void sendFileTCP(string IP_addr, Int32 port_number, string nome_file)
-        {
-            /**************
-            cancellare la classe progressbar già implementata in windows
-            **********/
-            try
+            while (true)
             {
-                TcpClient client = new TcpClient(IP_addr, port_number);
-                NetworkStream netstream = client.GetStream();
-                int BufferSize = 1024;/**dimensione del pacchetto inviato**/
-
-                FileStream Fs = new FileStream(nome_file, FileMode.Open, FileAccess.Read);/**apro il file in lettura**/
-                int packets_number = Convert.ToInt32
-         (Math.Ceiling(Convert.ToDouble(Fs.Length) / Convert.ToDouble(BufferSize)));
-                /**il numero di pacchetti da inviare è pari alla grandezza totale del file diviso la 
-            grandezza del buffer**/
-
-                ProgressBar progress_bar = new ProgressBar();
-                progress_bar.setMaximum(packets_number);
-
-                int lunghezza_file = (int)Fs.Length, dim_pacchetto_corrente, cont = 0;
-                
-                for(int i = 0; i < packets_number; i++)
+                try
                 {
-                    if (lunghezza_file > BufferSize)
-                    {
-                        dim_pacchetto_corrente = BufferSize;
-                        lunghezza_file = lunghezza_file - dim_pacchetto_corrente;
-                    }
-                    else
-                        dim_pacchetto_corrente = lunghezza_file;
-                    byte[] SendingBuffer = new byte[dim_pacchetto_corrente];
-                    Fs.Read(SendingBuffer, 0, dim_pacchetto_corrente);
-                    netstream.Write(SendingBuffer, 0, (int)SendingBuffer.Length);
-                    /*if (progressBar1.Value >= progressBar1.Maximum)
-                        progressBar1.Value = progressBar1.Minimum;
-                    progressBar1.PerformStep();*/
+                    byte[] serverResponseBytes = newClient.Receive(ref remoteServer);
 
+                    string serverResponse = Encoding.ASCII.GetString(serverResponseBytes);
+
+                    string formattedString = string.Format("Client: Received '{0}' from {1}", serverResponse, remoteServer.Address.ToString());
+
+                    MessageBox.Show(formattedString);
                 }
-                
-                 
+                catch (SocketException e)
+                {
+                    MessageBox.Show(e.Message + "Timeout expired!");
+                    break;
+                }
+                catch(Exception e)
+                {
+                    MessageBox.Show("Exception: "+e.Message);
+                    break;
+                }
             }
-            catch(Exception e)
-
-            {
-
-
-            }
-            finally {   }
-        }
-    
-            MessageBox.Show(formattedString);
-
-            newClient.Close();
         }
     }
 }
